261104adb4c9ff99f547c9a40f1563f5c328fe6b
Fiskgeo <rncrus@gmail.com>
2025-12-14 09:26:48 +0300

    final_proj_commits

--- page/ApiPage.py
+++ page/ApiPage.py
@@ -5,7 +5,16 @@ class ApiPage:
 
    def __init__(self, base_url, token):
        self.base_url = base_url
-       self._token = token
+       self.token = token
 
    def _get_headers(self):
-       return {"Content-type": "application/json"}
+       return {"Content-type": "application/json",
+               "Authorization": f"Bearer {self.token}"}
+
+   @allure.step("Поиск продукта по фразе: {phrase}")
+   def search_product(self, phrase, customer_city_id = 54):
+       params = {"customerCityId": customer_city_id,
+                 "phrase": phrase}
+       response = requests.get(self.base_url, headers=self._get_headers(), params=params)
+       allure.attach(response.text, "Ответ API")
+       return response
